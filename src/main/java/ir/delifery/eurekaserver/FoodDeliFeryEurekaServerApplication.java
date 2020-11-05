4fb823aafa68d756e25fe4689cb82dcc5def3cbc
Farid Shahy <fshahy@gmail.com>
2020-11-05 12:12:52 +0330

    init maven project.

---
+++ src/main/java/ir/delifery/eurekaserver/FoodDeliFeryEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ir.delifery.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliFeryEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliFeryEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/delifery/eurekaserver/FoodDeliFeryEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.delifery.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliFeryEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
